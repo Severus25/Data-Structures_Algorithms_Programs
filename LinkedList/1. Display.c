761528a8aa60a25823b9dbe3a2856aa902c484a3
Abhijai Rajawat <77570887+Severus25@users.noreply.github.com>
2023-06-24 21:45:25 +0530

    Update 1. Display.c

--- LinkedList/1. Display.c
+++ LinkedList/1. Display.c
@@ -5,7 +5,7 @@ struct Node
 {
 	int data;
 	struct Node *next;
-}*first = NULL;					//Global Pointer //Can be directly accessed and can pass as parameter
+}*first = NULL;					//Global Pointer - Can be directly accessed and can pass as parameter
 
 void create(int A[], int n)
 {
@@ -14,17 +14,17 @@ void create(int A[], int n)
 	struct Node *last;			//pointer to point at last node //helps in adding new node in end of linked list
 	
 	first = (struct Node *)malloc(sizeof(struct Node));        //creates space for first node in heap
-	first->data = A[0];										  //data part is assigned as first element of the array
-	first->next = NULL;										  //next pointer points to NULL only
-	last = first;										     //last pointer made to point at first node //helps in linking nodes [V. Imp]
+	first->data = A[0];					   //data part is assigned as first element of the array
+	first->next = NULL;					   //next pointer points to NULL only
+	last = first;						   //last pointer made to point at first node //helps in linking nodes [V. Imp]
 	
-	for(i=1; i<n; i++)										//starting from 1 as already 0th element is created
+	for(i=1; i<n; i++)					   //starting from 1 as already 0th element is created
 	{
-		t = (struct Node *)malloc(sizeof(struct Node));     //everytime a new space is created in heap for a new node represented by t
-		t->data = A[i];										//data part is assigned according to array element
-		t->next = NULL;										//everytime the node is created its next points to NULL as there is no node after that
-		last->next = t;										//helps in linking the previous node with the newly created node
-		last = t;											//last refreshes its pointer to the newly created node that will help again in linking a newly created node in next iteration
+		t = (struct Node *)malloc(sizeof(struct Node));     //every time a new space is created in heap for a new node represented by t
+		t->data = A[i];					    //data part is assigned according to array element
+		t->next = NULL;					    //every time the node is created its next points to NULL as there is no node after that
+		last->next = t;					    //helps in linking the previous node with the newly created node
+		last = t;	//last refreshes its pointer to the newly created node that will help again in linking a newly created node in next iteration
 	}
 	
 }

5556a90681e161f5855abc51894ce7fbf49f35a5
Abhijai Rajawat <77570887+Severus25@users.noreply.github.com>
2023-09-13 13:08:32 +0530

    Create 4. Sort The Array.cpp

---
+++ Array/Programs/4. Sort The Array.cpp
@@ -0,0 +1,27 @@
+// Problem Statement:
+// Given a random set of numbers, Print them in sorted order.
+
+// Example 1:
+// Input:
+// N = 4
+// arr[] = {1, 5, 3, 2}
+// Output: {1, 2, 3, 5}
+// Explanation: After sorting array will 
+// be like {1, 2, 3, 5}.
+
+// Example 2:
+// Input:
+// N = 2
+// arr[] = {3, 1}
+// Output: {1, 3}
+// Explanation: After sorting array will
+// be like {1, 3}.
+
+// Problem Solution:
+class Solution{
+    public:
+    vector<int> sortArr(vector<int>arr, int n){
+    sort(arr.begin(), arr.end());
+    return arr;
+    }
+};
